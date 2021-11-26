81d89984aa1bd3d85ed9774322e4eebd56788262
F-Papa <francopapa21@gmail.com>
2021-11-26 13:33:38 -0300

    hola mundo

---
+++ src/main/java/com/psa/proyectos/ProyectosApplication.java
@@ -0,0 +1,24 @@
+package com.psa.proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectosApplication.class, args);
+	}
+
+}
+
+@RestController
+class HolaMundo {
+
+	@GetMapping("/")
+	String HolaMundo(){
+		return "Hola, Mundo";
+	}
+}
