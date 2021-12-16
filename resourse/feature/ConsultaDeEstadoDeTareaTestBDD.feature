f28ae0dd3964c09deef226649f402db38f21daf2
F-Papa <francopapa21@gmail.com>
2021-12-16 17:28:07 -0300

    BDD

--- resourse/feature/ConsultaDeEstadoDeTareaTestBDD.feature
+++
@@ -1,9 +0,0 @@
-Feature: Consultar estado de tarea
-  Como usuario quiero conocer el estado de una tarea para saber si tengo que trabajar en ella
-
-  Scenario: Consulta de estado de tarea correcta
-    Given se seleccionó una tarea
-    When indica que desea conocer su estado
-    Then el sistema informa al usuario cuál es el estado de la misma
-
-    
---
+++ src/test/java/com/psa/proyectos/MyStepDefs.java
@@ -0,0 +1,142 @@
+package com.psa.proyectos;
+
+import io.cucumber.java.en.*;
+
+public class MyStepDefs {
+    @Given("existe un proyecto")
+    public void existeUnProyecto() {
+
+    }
+
+    @But("no tiene lider")
+    public void noTieneLider() {
+    }
+
+    @When("indica que desea agregarlo en forma de lider")
+    public void indicaQueDeseaAgregarloEnFormaDeLider() {
+    }
+
+    @Then("el sistema agrega al usuario seleccionado al proyecto en forma de lider")
+    public void elSistemaAgregaAlUsuarioSeleccionadoAlProyectoEnFormaDeLider() {
+    }
+
+    @And("el sistema le informa al usuario")
+    public void elSistemaLeInformaAlUsuario() {
+    }
+
+    @And("el sistema le informa a quién lo agrego del exito")
+    public void elSistemaLeInformaAQuiénLoAgregoDelExito() {
+    }
+
+    @And("tiene lider")
+    public void tieneLider() {
+    }
+
+    @Then("el sistema le informa a quién lo agrego que el proyecto ya tiene lider")
+    public void elSistemaLeInformaAQuiénLoAgregoQueElProyectoYaTieneLider() {
+    }
+
+    @Given("que el usuario ingresó un nombre para la tarea")
+    public void queElUsuarioIngresóUnNombreParaLaTarea() {
+    }
+
+    @And("las horas estimadas")
+    public void lasHorasEstimadas() {
+    }
+
+    @And("el proyecto al que pertenecerá")
+    public void elProyectoAlQuePertenecerá() {
+    }
+
+    @When("el usuario indica que desea crearla")
+    public void elUsuarioIndicaQueDeseaCrearla() {
+    }
+
+    @Then("el sistema registra la tarea")
+    public void elSistemaRegistraLaTarea() {
+    }
+
+    @And("la vincula al proyecto ingresado")
+    public void laVinculaAlProyectoIngresado() {
+    }
+
+    @And("informa a quienes fueron agregados")
+    public void informaAQuienesFueronAgregados() {
+    }
+
+    @And("informa a quien la creo del exito")
+    public void informaAQuienLaCreoDelExito() {
+    }
+
+    @But("no las horas estimadas")
+    public void noLasHorasEstimadas() {
+    }
+
+    @But("no el proyecto al que pertenecerá")
+    public void noElProyectoAlQuePertenecerá() {
+    }
+
+    @Then("el sistema no registra la tarea")
+    public void elSistemaNoRegistraLaTarea() {
+    }
+
+    @And("informa a quien la creo de la nesesidad de vincularla a un proyecto")
+    public void informaAQuienLaCreoDeLaNesesidadDeVincularlaAUnProyecto() {
+    }
+
+    @Given("se selecciona un proyecto sin miembros")
+    public void seSeleccionaUnProyectoSinMiembros() {
+    }
+
+    @When("indica que desea consultar sus miembros")
+    public void indicaQueDeseaConsultarSusMiembros() {
+    }
+
+    @Then("el sistema informa al usuario que el proyecto no tiene miembros")
+    public void elSistemaInformaAlUsuarioQueElProyectoNoTieneMiembros() {
+    }
+
+    @Given("se selecciona un proyecto con miembros")
+    public void seSeleccionaUnProyectoConMiembros() {
+    }
+
+    @Then("el sistema informa al usuario quiénes son los miembros del proyecto")
+    public void elSistemaInformaAlUsuarioQuiénesSonLosMiembrosDelProyecto() {
+    }
+
+    @Given("se seleccionó una tarea")
+    public void seSeleccionóUnaTarea() {
+    }
+
+    @When("indica que desea conocer su estado")
+    public void indicaQueDeseaConocerSuEstado() {
+    }
+
+    @Then("el sistema informa al usuario cuál es el estado de la misma")
+    public void elSistemaInformaAlUsuarioCuálEsElEstadoDeLaMisma() {
+    }
+
+    @Given("que el usuario ingresó un nombre:")
+    public void queElUsuarioIngresóUnNombre() {
+    }
+
+    @When("indica que desea crear el proyecto")
+    public void indicaQueDeseaCrearElProyecto() {
+    }
+
+    @Then("el sistema crea el proyecto con la información incompleta. Finalmente notifica al usuario")
+    public void elSistemaCreaElProyectoConLaInformaciónIncompletaFinalmenteNotificaAlUsuario() {
+    }
+
+    @Given("que el usuario no ingresó un nombre")
+    public void queElUsuarioNoIngresóUnNombre() {
+    }
+
+    @Then("el sistema le avisa al usuario que debe ingresar un nombre para poder crear el proyecto")
+    public void elSistemaLeAvisaAlUsuarioQueDebeIngresarUnNombreParaPoderCrearElProyecto() {
+    }
+
+    @Then("el sistema crea el proyecto y notifica al usuario:")
+    public void elSistemaCreaElProyectoYNotificaAlUsuario() {
+    }
+}
